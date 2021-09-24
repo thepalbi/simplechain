fcee69609b9ec5d90ec9bfad03522b4a5f870bde
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-09-24 01:48:02 -0300

    Removing extra test code

--- blockchain/index.ts
+++ blockchain/index.ts
@@ -14,13 +14,3 @@ export class Blockchain {
 		return this.chain[this.chain.length - 1];
 	}
 }
-
-const chain = new Blockchain();
-for (let i = 0; i < 1000; i++) {
-	const lastBlock = chain.lastBlock();
-	console.log("Parent hash: %s Difficulty: %d", lastBlock.blockHeaders.parentHash, lastBlock.blockHeaders.difficulty);
-	const newBlock = Block.mineBlock({ lastBlock, beneficiary: "beneficiary" });
-	chain.addBlock({ block: newBlock });
-}
-
-console.log(JSON.stringify(chain));
