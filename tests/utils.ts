a71e1e5ddc88d5579b07625a95780c7a556a5c83
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-09-23 22:30:13 -0300

    Hash function implemented

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { sortCharacter } from "../util";
+import { hash, sortCharacter } from "../util";
 
 describe("Utils#sortCharacters", function () {
 	it("creates same strings for equal objects in differet order", function () {
@@ -14,5 +14,33 @@ describe("Utils#sortCharacters", function () {
 		const a = sortCharacter({ hola: 1, perro: "holis" });
 		const b = sortCharacter({ ble: 2, hola: 1 });
 		expect(a).to.be.not.equal(b);
+	});
+});
+
+describe("Utils#hash", function () {
+	it("Produces a keccak256 hash for some object", function () {
+		const obj = { hola: 1, hola2: "ble" };
+		const h = hash(obj);
+		expect(h).to.be.equal("714af2682d112e08c511311382dcd41b7f0ca45c2e989b2d92abfbc3e7a534f1");
+	});
+
+	it("Produces same hash for equal but not ordered fields objects", function () {
+		const obj1 = { hola: 1, hola2: "ble" };
+		const obj2 = { hola2: "ble", hola: 1, };
+		const h1 = hash(obj1);
+		const h2 = hash(obj2);
+		expect(h1).to.be.not.empty;
+		expect(h2).to.be.not.empty;
+		expect(h1).to.be.equal(h2);
+	});
+
+	it("Produces different hash for unequal objects", function () {
+		const obj1 = { hola: 1, hola2: "ble" };
+		const obj2 = { hola3: "bla", hola: 1, };
+		const h1 = hash(obj1);
+		const h2 = hash(obj2);
+		expect(h1).to.be.not.empty;
+		expect(h2).to.be.not.empty;
+		expect(h1).not.to.be.equal(h2);
 	});
 });
--- util/index.ts
+++ util/index.ts
@@ -1,3 +1,11 @@
+import { keccak256 } from "js-sha3";
+
 export function sortCharacter(data: any): string {
 	return JSON.stringify(data).split("").sort().join("");
+}
+
+export function hash(data: any): string {
+	let hash = keccak256.create();
+	hash.update(sortCharacter(data));
+	return hash.hex();
 }
