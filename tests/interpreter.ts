421dac2c26de24e9c2d0abc722921596543a5153
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-09-23 23:12:50 -0300

    Refactor and order test suites

--- tests/interpreter.ts
+++ tests/interpreter.ts
@@ -6,60 +6,64 @@ function runCodeAndExpectResult(code: CodeSymbol[], expectedResult: number) {
     expect(res).to.equal(expectedResult);
 }
 
-describe("Interpreter tests", () => {
-    let int = new Interpreter();
+describe("Interpreter", function () {
+    describe("Operations tests", function () {
+        let int = new Interpreter();
 
-    let generateCode = (a: number, b: number, arithOrCompOpCode: OpCode): CodeSymbol[] =>
-        ["PUSH", b, "PUSH", a, arithOrCompOpCode, "STOP"];
+        let generateCode = (a: number, b: number, arithOrCompOpCode: OpCode): CodeSymbol[] =>
+            ["PUSH", b, "PUSH", a, arithOrCompOpCode, "STOP"];
 
-    let cases: [number, number, OpCode, number, string?][] = [
-        [1, 3, "ADD", 4],
-        [3, 1, "SUB", 2],
-        [3, 1, "DIV", 3],
-        [3, 1, "LT", 0],
-        [5, 1, "GT", 1],
-        [1, 1, "AND", 1],
-        [0, 1, "AND", 0, "False scneario"],
-        [1, 0, "OR", 1],
-        [10, 12, "EQ", 0],
-        [12, 12, "EQ", 1, "Equal scneario"],
-    ];
-    cases.forEach(([a, b, arithOrCompOpCode, expectedRes, note]) => {
-        it(`Operation: ${arithOrCompOpCode.toString()}${note !== undefined ? " - ".concat(note) : ""}`,
-            () => {
-                let result = new Interpreter().runCode(generateCode(a, b, arithOrCompOpCode));
-                expect(result).to.equal(expectedRes);
-            })
+        let cases: [number, number, OpCode, number, string?][] = [
+            [1, 3, "ADD", 4],
+            [3, 1, "SUB", 2],
+            [3, 1, "DIV", 3],
+            [3, 1, "LT", 0],
+            [5, 1, "GT", 1],
+            [1, 1, "AND", 1],
+            [0, 1, "AND", 0, "False scneario"],
+            [1, 0, "OR", 1],
+            [10, 12, "EQ", 0],
+            [12, 12, "EQ", 1, "Equal scneario"],
+        ];
+        cases.forEach(([a, b, arithOrCompOpCode, expectedRes, note]) => {
+            it(`${arithOrCompOpCode.toString()}${note !== undefined ? " - ".concat(note) : ""}`,
+                () => {
+                    let result = new Interpreter().runCode(generateCode(a, b, arithOrCompOpCode));
+                    expect(result).to.equal(expectedRes);
+                })
+        });
+
+        it("PUSH", () =>
+            runCodeAndExpectResult(["PUSH", 5, "STOP"], 5));
+
+        it("PUSH - Operation with out of bounds error", () => {
+            expect(() => new Interpreter().runCode(["PUSH"])).to.throw("No element to PUSH")
+        });
+
+        it("JUMP - Alters return value", () =>
+            runCodeAndExpectResult(["PUSH", 6, "JUMP", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 4));
+
+        let jumpOutOfBoundsCases: [number, string][] = [[-5, "negative location"], [19, "greater than code size"]];
+        jumpOutOfBoundsCases.forEach(([outOfBoundsLocation, note]) =>
+            it(`JUMP - To out of bounds location fails - ${note}`, () =>
+                expect(() => new Interpreter().runCode(["PUSH", outOfBoundsLocation, "JUMP", "PUSH", 5, "STOP", "PUSH", 4, "STOP"])
+                ).to.throw("Out of bounds jump destination")
+            )
+        );
+
+        it("JUMPI - Takes jump", () =>
+            runCodeAndExpectResult(["PUSH", 8, "PUSH", 1, "JUMPI", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 4));
+
+        it("JUMPI - Doesnt takes jump", () =>
+            runCodeAndExpectResult(["PUSH", 8, "PUSH", 0, "JUMPI", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 5));
     });
 
-    it("Push operation", () =>
-        runCodeAndExpectResult(["PUSH", 5, "STOP"], 5));
-
-    it("Push operation with out of bounds error", () => {
-        expect(() => new Interpreter().runCode(["PUSH"])).to.throw("No element to PUSH")
+    describe("Border cases", function () {
+        it("Infinite loops should fail", function () {
+            //@ts-ignore
+            this.timeout(2000);
+            // TODO: The timeout above doesn't work as expected
+            expect(() => new Interpreter().runCode(["PUSH", 0, "JUMP"])).to.throw("Execution cound limit reached!");
+        });
     });
-
-    it("JUMP alters return value", () =>
-        runCodeAndExpectResult(["PUSH", 6, "JUMP", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 4));
-
-    let jumpOutOfBoundsCases: [number, string][] = [[-5, "negative location"], [19, "greater than code size"]];
-    jumpOutOfBoundsCases.forEach(([outOfBoundsLocation, note]) =>
-        it(`JUMP to out of bounds location fails - ${note}`, () =>
-            expect(() => new Interpreter().runCode(["PUSH", outOfBoundsLocation, "JUMP", "PUSH", 5, "STOP", "PUSH", 4, "STOP"])
-            ).to.throw("Out of bounds jump destination")
-        )
-    );
-
-    it("JUMPI takes jump", () =>
-        runCodeAndExpectResult(["PUSH", 8, "PUSH", 1, "JUMPI", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 4));
-
-    it("JUMPI doesnt takes jump", () =>
-        runCodeAndExpectResult(["PUSH", 8, "PUSH", 0, "JUMPI", "PUSH", 5, "STOP", "PUSH", 4, "STOP"], 5));
-
-    it("Infinit loops should fail", function () {
-        //@ts-ignore
-        this.timeout(2000);
-        // TODO: The timeout above doesn't work as expected
-        expect(() => new Interpreter().runCode(["PUSH", 0, "JUMP"])).to.throw("Execution cound limit reached!");
-    });
-})
+});
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,46 +1,48 @@
 import { expect } from "chai";
 import { hash, sortCharacter } from "../util";
 
-describe("Utils#sortCharacters", function () {
-	it("creates same strings for equal objects in differet order", function () {
-		const objA = { hola: 1, ble: 2 };
-		const objB = { ble: 2, hola: 1 };
-		const a = sortCharacter(objA);
-		const b = sortCharacter(objB);
-		expect(a).to.be.equal(b);
+describe("Utils", function () {
+	describe("sortCharacters()", function () {
+		it("creates same strings for equal objects in differet order", function () {
+			const objA = { hola: 1, ble: 2 };
+			const objB = { ble: 2, hola: 1 };
+			const a = sortCharacter(objA);
+			const b = sortCharacter(objB);
+			expect(a).to.be.equal(b);
+		});
+
+		it("create different string for different objects", function () {
+			const a = sortCharacter({ hola: 1, perro: "holis" });
+			const b = sortCharacter({ ble: 2, hola: 1 });
+			expect(a).to.be.not.equal(b);
+		});
 	});
 
-	it("create different string for different objects", function () {
-		const a = sortCharacter({ hola: 1, perro: "holis" });
-		const b = sortCharacter({ ble: 2, hola: 1 });
-		expect(a).to.be.not.equal(b);
-	});
-});
+	describe("hash()", function () {
+		it("Produces a keccak256 hash for some object", function () {
+			const obj = { hola: 1, hola2: "ble" };
+			const h = hash(obj);
+			expect(h).to.be.equal("714af2682d112e08c511311382dcd41b7f0ca45c2e989b2d92abfbc3e7a534f1");
+		});
 
-describe("Utils#hash", function () {
-	it("Produces a keccak256 hash for some object", function () {
-		const obj = { hola: 1, hola2: "ble" };
-		const h = hash(obj);
-		expect(h).to.be.equal("714af2682d112e08c511311382dcd41b7f0ca45c2e989b2d92abfbc3e7a534f1");
-	});
+		it("Produces same hash for equal but not ordered fields objects", function () {
+			const obj1 = { hola: 1, hola2: "ble" };
+			const obj2 = { hola2: "ble", hola: 1, };
+			const h1 = hash(obj1);
+			const h2 = hash(obj2);
+			expect(h1).to.be.not.empty;
+			expect(h2).to.be.not.empty;
+			expect(h1).to.be.equal(h2);
+		});
 
-	it("Produces same hash for equal but not ordered fields objects", function () {
-		const obj1 = { hola: 1, hola2: "ble" };
-		const obj2 = { hola2: "ble", hola: 1, };
-		const h1 = hash(obj1);
-		const h2 = hash(obj2);
-		expect(h1).to.be.not.empty;
-		expect(h2).to.be.not.empty;
-		expect(h1).to.be.equal(h2);
-	});
-
-	it("Produces different hash for unequal objects", function () {
-		const obj1 = { hola: 1, hola2: "ble" };
-		const obj2 = { hola3: "bla", hola: 1, };
-		const h1 = hash(obj1);
-		const h2 = hash(obj2);
-		expect(h1).to.be.not.empty;
-		expect(h2).to.be.not.empty;
-		expect(h1).not.to.be.equal(h2);
+		it("Produces different hash for unequal objects", function () {
+			const obj1 = { hola: 1, hola2: "ble" };
+			const obj2 = { hola3: "bla", hola: 1, };
+			const h1 = hash(obj1);
+			const h2 = hash(obj2);
+			expect(h1).to.be.not.empty;
+			expect(h2).to.be.not.empty;
+			expect(h1).not.to.be.equal(h2);
+		});
 	});
 });
